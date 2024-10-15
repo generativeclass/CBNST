7b29f8c0bbebd53ce205d35b07fcc99f76e0b7a3
generativeclass <145311557+generativeclass@users.noreply.github.com>
2024-10-15 23:22:47 +0530

    backward.c

--- backward.c
+++ backward.c
@@ -42,20 +42,6 @@ int main()
             y[j][i]=y[j][i-1]-y[j-1][i-1];
         }
     }
-    
-        // Check for uniformity in differences
-//    int isUniform = 1;
-//    for (int i = 1; i < n; i++) {
- //       if (y[i][1] - y[i - 1][1] != y[1][1] - y[0][1]) {
- //           isUniform = 0;
-  //          break;
- //       }
- //   }
-//
-  //  if (!isUniform) {
-        // printf("Error: The differences are not uniform.\n");
- //       return 1; // Exit with an error
-  //  }
 
     printf("Difference table : \n");
     for(int i=0;i<n;i++)
@@ -80,72 +66,3 @@ int main()
     printf("Value at %d is %0.4f\n",val,sum);  
     return 0;
 }
-
-
-
-
-
-
-
-// // Online C compiler to run C program online
-// #include <stdio.h>
-// float cal_u(float u, int n){
-//     float temp=u;
-//     for(int i=1;i<n;i++){
-//         temp*=(u+i);
-//     }
-//     return temp;
-// }
-// int fact(int n){
-//     int f=1;
-//     for(int i=2;i<=n;i++){
-//         f*=i;
-//     }
-//     return f;
-// }
-// int main() {
-//     // Write C code here
-//     printf("enter number of elements: ");
-//     int n;
-//     float val;
-//     scanf("%d",&n);
-//     float x[10],y[10][10];
-//     printf("enter values of x:\n");
-//     for(int i=0;i<n;i++){
-//         scanf("%f",&x[i]);
-//     }
-//     printf("enter values of y:\n");
-//     for(int i=0;i<n;i++){
-//         scanf("%f",&y[i][0]);
-//     }
-    
-//     // differcence table;
-//     for(int i=1;i<n;i++){
-//         for(int j=n-1;j>=i;j--){
-//             y[j][i]=y[j][i-1]-y[j-1][i-1];
-//         }
-//     }
-    
-//     printf("\nDifference table:\n");
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<=i;j++){
-//             printf("%f ",y[i][j]);
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
-//     printf("enter interpolation value: ");
-    
-//     scanf("%f",&val);
-    
-//     float u=(val-x[n-1])/(x[1]-x[0]);
-    
-//     float sum=y[n-1][0];
-//     for(int i=1;i<n;i++){
-//         sum+=(cal_u(u,i)*y[n-1][i])/fact(i);
-//     }
-    
-//     printf("Value at %.4f is %.4f\n", val, sum);
-
-//     return 0;
-// }
